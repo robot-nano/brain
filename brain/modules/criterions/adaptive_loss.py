f78c05798fe6b4ecd9b83f7002f9cec539d4a911
robot-nano <adaofu01@gmail.com>
2023-07-28 14:02:32 +0800

    adaptive_loss reduction metrics

--- brain/modules/criterions/adaptive_loss.py
+++ brain/modules/criterions/adaptive_loss.py
@@ -41,4 +41,25 @@ class AdaptiveLoss:
         }
         return loss, sample_size, logging_output
 
-    
+    def reduce_metrics(logging_outputs) -> None:
+        """Aggregate logging outputs from data parallel training."""
+        loss_sum = utils.item(sum(log.get("loss", 0) for log in logging_outputs))
+        ntokens = utils.item(sum(log.get("ntokens", 0) for log in logging_outputs))
+        sample_size = utils.item(
+            sum(log.get("sample_size", 0) for log in logging_outputs)
+        )
+
+        metrics.log_scalar(
+            "loss", loss_sum / sample_size / math.log(2), sample_size, round=3
+        )
+        if sample_size != ntokens:
+            metrics.log_scalar(
+                "nll_loss", loss_sum / ntokens / math.log(2), ntokens, round=3
+            )
+            metrics.log_derived(
+                "ppl", lambda meters: utils.get_perplexity(meters["nll_loss"].avg)
+            )
+        else:
+            metrics.log_derived(
+                "ppl", lambda meters: utils.get_perplexity(meters["loss"].avg)
+            )
