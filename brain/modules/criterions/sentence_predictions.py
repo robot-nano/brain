565ce3557c76c0222cd104c0d744434086c6469f
robot-nano <adaofu01@gmail.com>
2024-01-29 19:26:30 +0800

    sentence predictions

--- brain/modules/criterions/sentence_predictions.py
+++ brain/modules/criterions/sentence_predictions.py
@@ -26,3 +26,29 @@ def matthews_corrcoef(preds, labels):
     # make it consistent with other metrics taking (preds, labels) as input
     mcc = _matthews_corrcoef(labels, preds)
     return mcc
+
+
+class SentencePredictionCriterion(object):
+    def __init__(self, cfg: SentencePredictionConfig, task):
+        super().__init__(task)
+        self.classification_head_name = cfg.classification_head_name
+        self.regression_target = cfg.regression_target
+        self.keep_pred_and_targ = (
+            cfg.report_mcc or cfg.report_acc_and_f1 or cfg.report_pearson_and_spearman
+        )
+        self.report_mcc = cfg.report_mcc
+        self.report_acc_and_f1 = cfg.report_acc_and_f1
+        self.report_pearson_and_spearman = cfg.report_pearson_and_spearman
+        self.label_dict = task.
+
+    def forward(self, model, sample, reduce=True):
+        assert (
+            hasattr(model, "classification_heads")
+            and self.classification_head_name in model.classification_heads
+        ), "model must provide sentence classification head for --criterion=sentence_prediction"
+
+        logits, _ = model(
+            **sample["net_input"],
+            features_only=True,
+            classification_head_name=self.classification_head_name,
+        )
