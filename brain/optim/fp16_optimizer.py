1d0adbd9be567cc23145ea54eb7afe165ffc91b6
robot-nano <adaofu01@gmail.com>
2024-10-19 19:56:25 +0800

    add zero gradient function

--- brain/optim/fp16_optimizer.py
+++ brain/optim/fp16_optimizer.py
@@ -221,3 +221,24 @@ class _FP16OptimizerMixin(object):
             self.scaler.update()
 
         self._sync_fp32_params_to_fp16()
+
+    def zero_grad(self):
+        """Clears the gradients of all optimized parameters."""
+        for p in self.fp16_params:
+            p.grad = None
+        if self.has_flat_params:
+            if torch.is_tensor(self.fp32_params):
+                self.fp32_params.grad.zero_()
+            elif isinstance(self.fp32_params, dict):
+                for fp32_params in self.fp32_params.values():
+                    fp32_params.grad.zero_()
+            else:
+                raise RuntimeError("self.fp32_params must be a tensor or dict")
+        else:
+            for p32 in self.fp32_params:
+                if p32.grad is not None:
+                    p32.grad.zero_()
+        self._needs_sync = False
+
+        if self.scaler is not None:
+            self._multiply_factor = 1.0 / float(self.scaler.loss_scale)
